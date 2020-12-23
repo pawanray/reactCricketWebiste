84495fce71aa548da9777d3ad2a44c37e2655d9b
unknown <pawanray19@gmail.com>
2020-12-23 16:39:10 +0530

    Updates

---
+++ precache-manifest.07c2c59d59a00efbb853bf0977c86cf9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4583cd41524c470fa598b94c37c010e5",
+    "url": "/reactCricketWebiste/index.html"
+  },
+  {
+    "revision": "47366080c0f75ca7946a",
+    "url": "/reactCricketWebiste/static/css/main.825927c8.chunk.css"
+  },
+  {
+    "revision": "ab68e1d5f1759cc8ea7f",
+    "url": "/reactCricketWebiste/static/js/2.ccac3c74.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/reactCricketWebiste/static/js/2.ccac3c74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47366080c0f75ca7946a",
+    "url": "/reactCricketWebiste/static/js/main.1c6a7246.chunk.js"
+  },
+  {
+    "revision": "8ab3914c48eca4cc4847",
+    "url": "/reactCricketWebiste/static/js/runtime-main.a04fcd7e.js"
+  },
+  {
+    "revision": "80c5554592d2dfc20bfb14fd7c3ff94f",
+    "url": "/reactCricketWebiste/static/media/cicket1.80c55545.webp"
+  },
+  {
+    "revision": "ea87040a89c42085a9a30279fe5972d0",
+    "url": "/reactCricketWebiste/static/media/cicket2.ea87040a.webp"
+  },
+  {
+    "revision": "b91eacbbc43f79a653999a037b51eb39",
+    "url": "/reactCricketWebiste/static/media/news1.b91eacbb.webp"
+  },
+  {
+    "revision": "c9be3d670d67d799cb1794a69fc3c147",
+    "url": "/reactCricketWebiste/static/media/news2.c9be3d67.webp"
+  },
+  {
+    "revision": "a970da6642b8efc878f0ca9c6bc31542",
+    "url": "/reactCricketWebiste/static/media/news3.a970da66.webp"
+  },
+  {
+    "revision": "e16817a28079380d0e815212745074ae",
+    "url": "/reactCricketWebiste/static/media/news4.e16817a2.webp"
+  },
+  {
+    "revision": "a28776ccdb3e3471f336a2b57cf50275",
+    "url": "/reactCricketWebiste/static/media/teamLogo.a28776cc.webp"
+  }
+]);
